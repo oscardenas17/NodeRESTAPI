09491b56a018caddcf126e3190933569ac0acbce
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-30 15:48:20 -0500

    Rutas y controlador, primeros pasos

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
+import pkg from '../package.json'
+import productsRoutes from '../src/routes/products.routes'
+
 
 const app = express();
 
+
+app.set('pkg',pkg)
 app.use(morgan('dev'));
 
 
 app.get('/', (req,res) => {
-    res.json("welcome")
+    res.json({
+        name: app.get('pkg').name,
+        author: app.get('pkg').author,
+    })
 })
+
+app.use('/products',productsRoutes)
 
 export default app
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,1 +1,12 @@
-import Router from 'express';
+import {Router} from 'express';
+import {createProduct,deleteProductById,getProductById,getProducts,updateProductById}  from '../controllers/products.controller'
+
+const router = Router()
+
+router.post('/', createProduct )
+router.get('/', getProducts )
+router.get('/:productId', getProductById )
+router.put('/:productId', updateProductById )
+router.delete('/:productId', deleteProductById )
+
+export default router;
