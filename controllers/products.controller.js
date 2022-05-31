73675271ea5491ea4dee7105205d2112c5abe1af
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-30 19:18:18 -0500

    Update and Delete

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -21,9 +21,12 @@ export const getProductById =async  (req, res)=>{
 }
 
 export const updateProductById =async (req, res)=>{
-    
+   const updatedProduct= await Product.findByIdAndUpdate(req.params.productId, req.body,{new:true})
+    res.status(200).json(updatedProduct)
 }
 
 export const deleteProductById = async(req, res)=>{
-    
+    const {productId} = req.params;
+    await Product.findByIdAndDelete(productId)
+    res.status(204).json()
 }
